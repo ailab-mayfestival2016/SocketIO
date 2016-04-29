ba6171b892f03d9ff588568da298119c83cbd0c4
T-Kawai0922 <nogarddnaworcfoaet.1836624@gmail.com>
2016-04-29 20:10:37 +0900

    PythonおよびC++版の、Boost非依存版(Windows)を動作確認。Prebuildに追加。

---
+++ BuildForPythonStatic.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+set BUILD_TYPE=Release
+rem if "%~1"!="" set BUILD_TYPE=%1
+echo "Python Version"
+cp CMakeListsPythonStatic.txt CMakeLists.txt
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE:STRING=%BUILD_TYPE% ../
+cmake --build . --config Release --target install
+
+cd ../Python
+if exist sioclientCpp.dll (
+move /y sioclientCpp.dll sioclientCpp.pyd
+del sioclientCpp.lib
+)
+if exist sioclient_tlsCpp.dll (
+move /y sioclient_tlsCpp.dll sioclient_tlsCpp.pyd
+rem del sioclient_tlsCpp.lib
+)
